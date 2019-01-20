41001c66eccee6094a99639ef51ad3155bef7f23
Shixian Geng <gengs1@gengs1-5510.rose-hulman.edu>
2019-01-20 03:52:18 -0500

    fragments

--- Project/app/src/main/java/rose/cheny16/projectfragment/MainActivity.kt
+++ Project/app/src/main/java/rose/cheny16/projectfragment/MainActivity.kt
@@ -2,11 +2,52 @@ package rose.cheny16.projectfragment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), fragment_mainpage.IgetFt{
+
+    override fun getFt(): FragmentTransaction {
+        return supportFragmentManager.beginTransaction()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        var ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container,fragment_login(),"login")
+        ft.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                true
+            }
+            R.id.action_saveload -> {
+                var ft = supportFragmentManager.beginTransaction()
+                ft.replace(R.id.fragment_container,fragment_saveload(),"login")
+                ft.addToBackStack("list")
+                ft.commit()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
